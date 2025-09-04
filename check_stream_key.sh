a2a8a604c8c48f77c43443d575a1718d1533b99a
jolsho <joshuaolson13@gmail.com>
2025-09-04 16:50:40 -0700

    Remove nginx files from repo but keep locally

--- check_stream_key.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-# /usr/local/bin/check_stream_key.sh
-STREAM_KEY=""
-if [ "$1" != "$STREAM_KEY" ]; then
-    exit 1  # deny publishing
-fi
-exit 0
-
-sudo chmod +x /usr/local/bin/check_stream_key.sh
